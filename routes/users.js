d562392478946b4df1a9ab9173b4755b17bcca49
teddy <teddypolytech@gmail.com>
2019-04-16 16:35:26 +0200

    ajout

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var router = require('express').Router();
 var User = require('./../models/User');
+var Liste = require('../models/Liste');
+var Task = require('../models/Task');
 
 router.get('/',(req,res) => {
-  User.find({}).populate('listes').then(users => {
+  User.find({}).populate('users').then(users => {
     res.render('users/index.html', {users: users}) ;
   });
 });
